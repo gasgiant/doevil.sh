9e5b1e48c70128916b675aa9ddd581ab07878e99
Ivan Pensionerov <31007222+gasgiant@users.noreply.github.com>
2020-07-11 15:12:07 +0300

    TileEditor

---
+++ Assets/Scripts/Editor/TileEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Tile))]
+public class TileEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        serializedObject.Update();
+        Tile tile = target as Tile;
+        tile.Validate();
+        serializedObject.ApplyModifiedProperties();
+    }
+}
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,6 +5,46 @@ public class Tile : MonoBehaviour
     [HideInInspector]
     public Vector2Int Index;
     public TileType type = TileType.Free;
+
+    [SerializeField]
+    GameObject freeVisuals = null;
+    [SerializeField]
+    GameObject wallVisuals = null;
+    [SerializeField]
+    GameObject deathVisuals = null;
+    [SerializeField]
+    GameObject goalVisuals = null;
+
+    public void Validate()
+    {
+        switch (type)
+        {
+            case TileType.Free:
+                freeVisuals.SetActive(true);
+                wallVisuals.SetActive(false);
+                goalVisuals.SetActive(false);
+                deathVisuals.SetActive(false);
+                break;
+            case TileType.Wall:
+                freeVisuals.SetActive(false);
+                wallVisuals.SetActive(true);
+                goalVisuals.SetActive(false);
+                deathVisuals.SetActive(false);
+                break;
+            case TileType.Goal:
+                freeVisuals.SetActive(false);
+                wallVisuals.SetActive(false);
+                goalVisuals.SetActive(true);
+                deathVisuals.SetActive(false);
+                break;
+            case TileType.Death:
+                freeVisuals.SetActive(false);
+                wallVisuals.SetActive(false);
+                goalVisuals.SetActive(false);
+                deathVisuals.SetActive(true);
+                break;
+        }
+    }
 }
 
-public enum TileType { Free, Wall }
+public enum TileType { Free, Wall, Goal, Death }
